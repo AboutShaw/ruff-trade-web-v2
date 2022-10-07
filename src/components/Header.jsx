ac3e8808b3154499648214e86ece8800e2f0a7d1
AboutShaw <shawsi1988@gmail.com>
2022-10-07 16:20:05 +0100

    commented out code removed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import IndexPage from '../pages';
-import About from '../pages/about';
-import Contact from '../pages/contact';
-import Gallery from '../pages/gallery';
-import Gigs from '../pages/gigs';
-import Merch from '../pages/merch';
-import Music from '../pages/music';
 
 export default function Header(){
   return (
@@ -20,14 +13,6 @@ export default function Header(){
         <Link className='link' to="/gigs"> GIGS </Link>
         <Link className='link' to="/merch"> MERCH </Link>
         <Link className='link' to="/contact"> CONTACT </Link>
-
-        {/* <a className='link' href="/" path={IndexPage}>HOME</a>
-        <a className='link' href="/about" path={About}>ABOUT</a>
-        <a className='link' href="/gallery" path={Gallery}>GALLERY</a>
-        <a className='link' href="/music" path={Music}>MUSIC</a>
-        <a className='link' href="/gigs" path={Gigs}>GIGS</a>
-        <a className='link' href="/merch" path={Merch}>MERCH</a>
-        <a className='link' href="/contact" path={Contact}>CONTACT</a> */}
       </div>
       <div className='headerLower'/>
     </div>
